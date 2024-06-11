ce822c62b73e5a13b5d4411c779fb2d5a0393463
affekri <derfla.ak@gmail.com>
2024-06-11 17:12:32 +0200

    Fixed build errors

--- src/main/java/com/actualweather/actualweather/commands/ActualWeatherCommand.java
+++ src/main/java/com/actualweather/actualweather/commands/ActualWeatherCommand.java
@@ -1,21 +1,20 @@
 package com.actualweather.actualweather.commands;
 
+import com.actualweather.actualweather.ActualWeather;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public class ActualWeatherCommand implements CommandExecutor {
 
-    private final JavaPlugin plugin;
-
-    public ActualWeatherCommand(JavaPlugin plugin) {
-        this.plugin = plugin;
-    }
+    static Plugin plugin = ActualWeather.getInstance();
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (!sender.isOp()) {
             sender.sendMessage("You do not have permission to use this command.");
             return false;
